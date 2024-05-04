ea19a2ffd1470e031f4fbb31b18abe453fda02f3
insotheo <maxim.insotheo@gmail.com>
2024-05-04 18:36:21 +0400

    Project setup and WelcomeWindow

---
+++ AppTimeControl/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using System.IO;
+
+namespace AppTimeControl
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            if(!File.Exists(Path.Combine(Directory.GetCurrentDirectory(), "app_settings.settings")) ||
+                !File.Exists(Path.Combine(Directory.GetCurrentDirectory(), "app_data.json")))
+            {
+                WelcomeWindow welcomeWindow = new WelcomeWindow();
+                welcomeWindow.Show();
+                this.Close();
+            }
+            InitializeComponent();
+        }
+    }
+}
